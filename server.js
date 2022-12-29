e54752b18e87669d3a7c93a1c26d514053c8faff
leaderbb <hoangphuonga3k31@gmail.com>
2022-12-29 16:51:05 +0700

    add-routes

--- server.js
+++ server.js
@@ -1,34 +1,45 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-const data = require('./data');
+const data = require("./data");
+const seedRouter = require("./routes/seedRoutes");
+const productRouter = require("./routes/ProductRoutes");
+const userRouter = require("./routes/userRoutes");
+const orderRouter = require("./routes/orderRoutes");
+
+dotenv.config();
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/api/products', (req, res) => {
-    res.send(data.products);
+app.get("/api/keys/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
 });
 
-app.get('/api/products/slug/:slug', (req, res) => {
-    const product = data.products.find((x) => x.slug === req.params.slug);
-    if (product) {
-      res.send(product);
-    } else {
-      res.status(404).send({ message: 'Product Not Found' });
-    }
-  });
+app.use("/api/seed", seedRouter);
+app.use("/api/products", productRouter);
+app.use("/api/users", userRouter);
+app.use("/api/orders", orderRouter);
 
-  app.get('/api/products/:id', (req, res) => {
-    const product = data.products.find((x) => x._id === req.params.id);
-    if (product) {
-      res.send(product);
-    } else {
-      res.status(404).send({ message: 'Product Not Found' });
-    }
-  });
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`server at port ${port} is running...`)
+  console.log(`server at port ${port} is running...`);
 });
