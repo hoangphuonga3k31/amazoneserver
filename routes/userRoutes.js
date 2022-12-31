9651133ce2ed97ffc0938c455c02b6a4fb686046
leaderbb <hoangphuonga3k31@gmail.com>
2022-12-31 16:00:20 +0700

    add-routes-and-vercel.json

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcryptjs");
 const expressAsyncHandler = require("express-async-handler");
 
 const User = require("../models/userModel");
-const { generateToken } = require("../utils");
+const { generateToken, isAuth } = require("../utils");
 
 const userRouter = express.Router();
 
@@ -43,6 +43,31 @@ userRouter.post(
       isAdmin: user.isAdmin,
       token: generateToken(user),
     });
+  })
+);
+
+userRouter.put(
+  "/profile",
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+    if (user) {
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+      if (req.body.password) {
+        user.password = bcrypt.hashSync(req.body.password, 8);
+      }
+      const updatedUser = await user.save();
+      res.send({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        isAdmin: updatedUser.isAdmin,
+        token: generateToken(updatedUser),
+      });
+    } else {
+      res.status(401).send({ message: "User not found" });
+    }
   })
 );
 
